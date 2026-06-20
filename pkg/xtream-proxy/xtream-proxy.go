c21ce0199bc892db8a74248bcb06cc59fc92850e
ninjarevolutions1971 <danyleleriz@gmail.com>
2026-06-20 20:35:26 +0200

    Fix login function call by removing protocol prefix

--- pkg/xtream-proxy/xtream-proxy.go
+++ pkg/xtream-proxy/xtream-proxy.go
@@ -159,7 +159,7 @@ func (c *Client) Action(config *config.ProxyConfig, action string, q url.Values)
 		}
 		respBody, err = c.GetEPG(q["stream_id"][0])
 	default:
-		respBody, err = c.login(config.User.String(), config.Password.String(), protocol+"://"+config.HostConfig.Hostname, config.AdvertisedPort, protocol)
+		respBody, err = c.login(config.User.String(), config.Password.String(), config.HostConfig.Hostname, config.AdvertisedPort, protocol)
 	}
 
 	return
